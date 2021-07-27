e18b07911e0388745afc50bb46884b0373a7e898
Naadborole <naad.borole2000@gmail.com>
2021-07-27 14:53:03 +0530

    changed port

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const cors = require('cors'); 
 
 const {userRoute} = require("./UserRoutes/userRoutes.js");
-
+const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(express.json());
 //app.use(bodyParser.json());
@@ -11,5 +11,5 @@ app.use(express.json());
 app.use(cors());
 
 app.use('/user',userRoute);
-app.listen(5000);
+app.listen(PORT);
 
